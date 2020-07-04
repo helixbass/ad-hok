4e774685327590f7f97b599bf117ecac0420204a
Julian Rosse <julian@helixbass.net>
2020-07-04 12:00:06 -0400

    addWrapper() typechecking

---
+++ src/addWrapper.tsx
@@ -0,0 +1,41 @@
+import React, {ReactElement, ComponentType} from 'react'
+
+import {CurriedPropsAdder} from 'helperTypes'
+
+const markerPropertyName = '__ad-hok-addWrapper'
+
+export const isAddWrapper = (func: Function): boolean =>
+  markerPropertyName in func
+
+type AddWrapperCallback<TAdditionalProps, TProps> = (
+  render: (additionalProps?: TAdditionalProps) => ReactElement | null,
+  props: TProps,
+) => ReactElement | null
+
+type AddWrapperPublishedType = <TAdditionalProps, TProps>(
+  callback: AddWrapperCallback<TAdditionalProps, TProps>,
+) => CurriedPropsAdder<TProps, TAdditionalProps>
+
+export const addWrapper = <TProps, TAdditionalProps>(
+  callback: AddWrapperCallback<TAdditionalProps, TProps>,
+): ((
+  Component: ComponentType<TProps & TAdditionalProps>,
+) => (props: TProps) => ReactElement | null) => {
+  const ret = (Component: ComponentType<TProps & TAdditionalProps>) => (
+    props: TProps,
+  ) =>
+    callback(
+      (additionalProps) => (
+        <Component
+          {...props}
+          {...((additionalProps ?? {}) as TAdditionalProps)}
+        />
+      ),
+      props,
+    )
+  ;(ret as any)[markerPropertyName] = true
+  return ret
+}
+
+const addWrapperPublishedType = addWrapper as AddWrapperPublishedType
+export default addWrapperPublishedType
