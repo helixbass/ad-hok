e13fd9cfef9262fb2b76166a2c0520f3db55c33b
Julian Rosse <julian@helixbass.net>
2020-07-04 09:35:16 -0400

    addState() typechecking

---
+++ src/addState.tsx
@@ -0,0 +1,49 @@
+import {useState} from 'react'
+
+import isFunction from 'utils/isFunction'
+import useMemoized from 'utils/useMemoized'
+import {ValueOrFunctionOfProps, CurriedPropsAdder} from 'helperTypes'
+
+type AddStateType = <
+  TState,
+  TStateName extends string,
+  TStateUpdaterName extends string,
+  TProps
+>(
+  stateName: TStateName,
+  stateUpdaterName: TStateUpdaterName,
+  initialState: ValueOrFunctionOfProps<TState, TProps>,
+) => CurriedPropsAdder<
+  TProps,
+  {[stateName in TStateName]: TState} &
+    {
+      [stateUpdaterName in TStateUpdaterName]: (
+        state: TState | ((prevState: TState) => TState),
+      ) => void
+    }
+>
+
+const addState: AddStateType = (name, setterName, initial) => (props) => {
+  const computedInitial = useMemoized(
+    () => (isFunction(initial) ? initial(props) : initial),
+    [],
+  )
+  const [state, setter] = useState(computedInitial)
+  type TState = typeof state
+  type TStateName = typeof name
+  type TStateUpdaterName = typeof setterName
+  type TProps = typeof props
+  return {
+    ...props,
+    [name]: state,
+    [setterName]: setter,
+  } as TProps &
+    {
+      [stateName in TStateName]: TState
+    } &
+    {
+      [stateUpdaterName in TStateUpdaterName]: typeof setter
+    }
+}
+
+export default addState
