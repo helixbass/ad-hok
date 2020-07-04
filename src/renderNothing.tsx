c51f99a93bf746b0bc62463088b80ed91dd47fc2
Julian Rosse <julian@helixbass.net>
2020-07-04 10:43:22 -0400

    renderNothing() typechecking

---
+++ src/renderNothing.tsx
@@ -0,0 +1,11 @@
+import {UnchangedProps} from 'helperTypes'
+
+const nonce = {}
+
+export const isRenderNothing = (value: unknown): boolean => value === nonce
+
+type RenderNothingType = <TProps>() => UnchangedProps<TProps>
+
+const renderNothing: RenderNothingType = () => (props) => nonce as typeof props
+
+export default renderNothing
