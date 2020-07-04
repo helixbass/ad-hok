59acdbbe20b042c242a6d0899718ed7f74b25f6d
Julian Rosse <julian@helixbass.net>
2020-07-04 10:39:48 -0400

    returns() typechecking

---
+++ src/returns.tsx
@@ -0,0 +1,23 @@
+import {UnchangedProps} from 'helperTypes'
+
+const key = '__ad-hok-returns'
+
+export const isReturns = (props: {}): [unknown] | false => {
+  try {
+    if (!(key in props)) return false
+    return [(props as any)[key]]
+  } catch {
+    return false
+  }
+}
+
+type ReturnsType = <TProps>(
+  callback: (props: TProps) => unknown,
+) => UnchangedProps<TProps>
+
+const returns: ReturnsType = (callback) => (props) =>
+  (({
+    [key]: callback(props),
+  } as unknown) as typeof props)
+
+export default returns
