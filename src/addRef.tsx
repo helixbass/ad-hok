76da564f8947704a4ea2bc7d55446cfb6f723339
Julian Rosse <julian@helixbass.net>
2020-07-04 12:06:57 -0400

    addRef() typechecking

---
+++ src/addRef.tsx
@@ -0,0 +1,35 @@
+import {useRef, MutableRefObject} from 'react'
+
+import isFunction from 'utils/isFunction'
+import useMemoized from 'utils/useMemoized'
+import {ValueOrFunctionOfProps, CurriedPropsAdder} from 'helperTypes'
+
+type AddRefType = <TRefName extends string, TRefValue, TProps>(
+  refName: TRefName,
+  initialValue: ValueOrFunctionOfProps<TRefValue, TProps>,
+) => CurriedPropsAdder<
+  TProps,
+  {
+    [refName in TRefName]: MutableRefObject<TRefValue>
+  }
+>
+
+const addRef: AddRefType = (name, initialValue) => (props) => {
+  const computedInitialValue = useMemoized(
+    () => (isFunction(initialValue) ? initialValue(props) : initialValue),
+    [],
+  )
+  const ref = useRef(computedInitialValue)
+  type TRefName = typeof name
+  type TRefValue = typeof computedInitialValue
+  type TProps = typeof props
+  return {
+    ...props,
+    [name]: ref,
+  } as TProps &
+    {
+      [refName in TRefName]: MutableRefObject<TRefValue>
+    }
+}
+
+export default addRef
