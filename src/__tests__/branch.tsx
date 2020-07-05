f0c11a1c15672cb537c5ab4d8cb32a8cb2829dbc
Julian Rosse <julian@helixbass.net>
2020-07-04 20:56:06 -0400

    branch() tets

--- src/__tests__/branch.tsx
+++ src/__tests__/branch.tsx
@@ -5,74 +5,96 @@ import '@testing-library/jest-dom/extend-expect'
 
 import {branch, flowMax, renderNothing, addStateHandlers, addState} from '..'
 
-Comp = flowMax(
+const Comp: FC<{
+  a: number
+  testId: string
+}> = flowMax(
   branch(
-    ({a}) -> a > 2
-    (props) -> {...props, a: 999}
-    (props) -> {...props, a: -888}
-  )
-,
-  ({a, testId}) ->
-    <div data-testid={testId}>{a}</div>
+    ({a}) => a > 2,
+    (props) => ({...props, b: 999}),
+    (props) => ({...props, b: -888}),
+  ),
+  ({b, testId}) => <div data-testid={testId}>{b}</div>,
 )
 
-Brancher = flowMax(
-  addStateHandlers {abort: no}, toggleAbort: ({abort}) -> -> abort: not abort
-  branch (({abort}) -> abort), renderNothing()
-  addState 'unused', 'setUnused'
-  ({a, testId, toggleAbort}) ->
-    <div>
-      <div data-testid={testId}>{a}</div>
-      <button onClick={toggleAbort}>toggle abort</button>
-    </div>
-)
-
-Brancher2 = flowMax(
-  branch (({abort}) -> abort), renderNothing()
-  addState 'unused', 'setUnused'
-  ({a, testId, toggleAbort}) ->
-    <div>
-      <div data-testid={testId}>{a}</div>
-      <button onClick={toggleAbort}>toggle abort</button>
-    </div>
-)
-
-describe 'branch', ->
-  test 'works when test passes', ->
-    testId = 'pass'
-    {getByTestId} = render <Comp a={3} testId={testId} />
-    expect(getByTestId testId).toHaveTextContent '999'
-
-  test 'works when test fails', ->
-    testId = 'fail'
-    {getByTestId} = render <Comp a={1} testId={testId} />
-    expect(getByTestId testId).toHaveTextContent '-888'
-
-  test 'works with renderNothing()', ->
-    testId = 'renderNothing-branch'
+const Brancher: FC<{
+  a: number
+  testId: string
+}> = flowMax(
+  addStateHandlers(
     {
-      getByTestId
-      getByText
-      queryByTestId
-    } = render <Brancher a={1} testId={testId} />
-    expect(getByTestId testId).toHaveTextContent '1'
-    fireEvent.click getByText /toggle abort/
-    expect(queryByTestId testId).toBeNull()
+      abort: false,
+    },
+    {
+      toggleAbort: ({abort}) => () => ({
+        abort: !abort,
+      }),
+    },
+  ),
+  branch(({abort}) => abort, renderNothing()),
+  addState('unused', 'setUnused', null),
+  ({a, testId, toggleAbort}) => (
+    <div>
+      <div data-testid={testId}>{a}</div>
+      <button onClick={toggleAbort}>toggle abort</button>
+    </div>
+  ),
+)
 
-  test 'works with renderNothing() when initially rendering nothing', ->
-    testId = 'renderNothing-branch-initial-abort'
-    OuterState = flow(
-      addStateHandlers
-        abort: yes
-      ,
-        toggleAbort: ({abort}) -> -> abort: not abort
-      ({abort, toggleAbort}) ->
+const Brancher2: FC<{
+  abort: boolean
+  a: number
+  testId: string
+}> = flowMax(
+  branch(({abort}) => abort, renderNothing()),
+  addState('unused', 'setUnused', null),
+  ({a, testId}) => <div data-testid={testId}>{a}</div>,
+)
+
+describe('branch', () => {
+  test('works when test passes', () => {
+    const testId = 'pass'
+    render(<Comp a={3} testId={testId} />)
+    expect(screen.getByTestId(testId)).toHaveTextContent('999')
+  })
+
+  test('works when test fails', () => {
+    const testId = 'fail'
+    render(<Comp a={1} testId={testId} />)
+    expect(screen.getByTestId(testId)).toHaveTextContent('-888')
+  })
+
+  test('works with renderNothing()', () => {
+    const testId = 'renderNothing-branch'
+    render(<Brancher a={1} testId={testId} />)
+    expect(screen.getByTestId(testId)).toHaveTextContent('1')
+    fireEvent.click(screen.getByText(/toggle abort/))
+    expect(screen.queryByTestId(testId)).toBeNull()
+  })
+
+  test('works with renderNothing() when initially rendering nothing', () => {
+    const testId = 'renderNothing-branch-initial-abort'
+    const OuterState: FC = flowMax(
+      addStateHandlers(
+        {
+          abort: true,
+        },
+        {
+          toggleAbort: ({abort}) => () => ({
+            abort: !abort,
+          }),
+        },
+      ),
+      ({abort, toggleAbort}) => (
         <div>
           <Brancher2 abort={abort} a={2} testId={testId} />
           <button onClick={toggleAbort}>toggle abort</button>
         </div>
+      ),
     )
-    {getByTestId, getByText, queryByTestId} = render <OuterState />
-    expect(queryByTestId testId).toBeNull()
-    fireEvent.click getByText /toggle abort/
-    expect(getByTestId testId).toHaveTextContent '2'
+    render(<OuterState />)
+    expect(screen.queryByTestId(testId)).toBeNull()
+    fireEvent.click(screen.getByText(/toggle abort/))
+    expect(screen.getByTestId(testId)).toHaveTextContent('2')
+  })
+})
